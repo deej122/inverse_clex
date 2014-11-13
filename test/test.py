a5d7828822f1e9ae450b071f85151968e4353912
kris789ten <kige@umich.edu>
2014-11-13 00:22:15 -0500

    added plottin.cpp

--- test/test.py
+++ test/test.py
@@ -3,5 +3,10 @@ import json
 json_data=open("monte_carlo_calcs.json").read()
 
 result = json.loads(json_data)
+num_pass = len(result["Data_by_pass"]) - 1
+#need to change this if more than 2 species
+avg_spin = abs(result["Data_by_pass"][num_pass]["num_species"][0]*result["Species"][0] + result["Data_by_pass"][num_pass]["num_species"][1]*result["Species"][1])/num_pass
 
-print result["Data_by_pass"][0]["num_species"]
+print avg_spin
+
+#result["Data_by_pass"][100]["num_species"]
