c6fa75a14877016378d071349de5b439c0c30194
kris789ten <kige@umich.edu>
2014-11-13 00:39:47 -0500

    edited python code

--- test/test.py
+++ test/test.py
@@ -7,6 +7,8 @@ num_pass = len(result["Data_by_pass"]) - 1
 #need to change this if more than 2 species
 avg_spin = abs(result["Data_by_pass"][num_pass]["num_species"][0]*result["Species"][0] + result["Data_by_pass"][num_pass]["num_species"][1]*result["Species"][1])/num_pass
 
+
+
 print avg_spin
 
 #result["Data_by_pass"][100]["num_species"]
