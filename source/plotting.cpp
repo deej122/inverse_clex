695d2f649bf6c2c313285c7a0f4f5bd2567ba528
DJ Jagannathan <desjag@gmail.com>
2014-11-15 14:46:44 -0500

    calculating average of squares, absolute value, squared average and pulling data in python test file. still need to plot

--- source/plotting.cpp
+++ source/plotting.cpp
@@ -34,14 +34,22 @@ int main()
 
 	int i,j;
 
+	double mean_spin_abs = 0;
 	double mean_spin = 0;
-	double ensemble_average = 0;
+	double mean_spin_square = 0;
+
+	double ensemble_average_abs = 0;
+	double ensemble_average_squared = 0;
+	double ensemble_average_squares = 0;
 
 	cout << 'writing json' << endl;
 
 	for (temp=0.001; temp<=1000000000; temp = temp*10)
 	{
+		double mean_spin_sum_abs = 0;
 		double mean_spin_sum = 0;
+		double mean_spin_sum_squares = 0;
+
 		for(pass_count=1; pass_count <= num_passes; pass_count++)
 		{
 			double spin_sum = 0;
@@ -54,16 +62,29 @@ int main()
 				}
 
 			}
-			//add squares
-			//add non-absolute value
-			mean_spin = abs(spin_sum/(matrix.size()*matrix[0].size()));
+			//absolute value of mean spin
+			mean_spin_abs = abs(spin_sum/(matrix.size()*matrix[0].size()));
+			//non-absolute value of mean spin
+			mean_spin = spin_sum/(matrix.size()*matrix[0].size());
+			//square of mean spin
+			mean_spin_square = pow((spin_sum/(matrix.size()*matrix[0].size())), 2);
 
+			//sum of absolute value mean spins
+			mean_spin_sum_abs += mean_spin_abs;
+			//sum of non-absolute value mean spins
 			mean_spin_sum += mean_spin;
+			//sum of squared mean spins
+			mean_spin_sum_squares += mean_spin_square;
 		}
 
 		//for non-squared, calculate average and then square
 		//calculate average for non-abs as well
-		ensemble_average = mean_spin_sum/(num_passes);
+		ensemble_average_abs = mean_spin_sum_abs/(num_passes);
+		//Squared average for non-squared mean sum
+		ensemble_average_squared = mean_spin_sum/(num_passes);
+		ensemble_average_squared = pow(ensemble_average_squared, 2);
+		//Average for squared mean sum
+		ensemble_average_squares = mean_spin_sum_squares/(num_passes);
 
 		cout << "begin" << endl;
 		cout << "index: " << index << endl;
@@ -74,7 +95,9 @@ int main()
 		json_out["plotting_data"][index].push_back(temp);
 		cout << "dos" << endl;
 		// json_out["plotting_data"][index]["average"] = average;
-		json_out["plotting_data"][index].push_back(ensemble_average);
+		json_out["plotting_data"][index].push_back(ensemble_average_abs);
+		json_out["plotting_data"][index].push_back(ensemble_average_squared);
+		json_out["plotting_data"][index].push_back(ensemble_average_squares);
 		cout << "tres" << endl;
 
 		index += 1;
--- test/test.py
+++ test/test.py
@@ -1,14 +1,24 @@
 import json
 
-json_data=open("monte_carlo_calcs.json").read()
+json_data = open("plotting_data.json").read()
 
 result = json.loads(json_data)
-num_pass = len(result["Data_by_pass"]) - 1
-#need to change this if more than 2 species
-avg_spin = abs(result["Data_by_pass"][num_pass]["num_species"][0]*result["Species"][0] + result["Data_by_pass"][num_pass]["num_species"][1]*result["Species"][1])/num_pass
+
+length = len(result["plotting_data"])
+
+temperature = []
+ensemble_average_absolute = []
+ensemble_average_squared = []
+ensemble_average_squares = []
+
+for i in range(0, length):
+	temperature.append(result["plotting_data"][i][0])
+	ensemble_average_absolute.append(result["plotting_data"][i][1])
+	ensemble_average_squared.append(result["plotting_data"][i][2])
+	ensemble_average_squares.append(result["plotting_data"][i][3])
 
 
-
-print avg_spin
-
-#result["Data_by_pass"][100]["num_species"]
+print "T: ", temperature
+print "Absolute Average: ", ensemble_average_absolute
+print "Squared Average: ", ensemble_average_squared
+print "Average of Squares: ", ensemble_average_squares
