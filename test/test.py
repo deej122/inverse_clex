2d9d5064dd23a7b08861f59c970b88d11d6027b2
kris789ten <kige@umich.edu>
2014-11-17 17:33:04 -0500

    working on python graphs

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 import json
+import matplotlib.pyplot as plt
 
 json_data = open("plotting_data.json").read()
 
@@ -16,6 +17,12 @@ for i in range(0, length):
 	ensemble_average_absolute.append(result["plotting_data"][i][1])
 	ensemble_average_squared.append(result["plotting_data"][i][2])
 	ensemble_average_squares.append(result["plotting_data"][i][3])
+	x = result["plotting_data"][i][0]
+	y1 = result["plotting_data"][i][1]
+	y2 = result["plotting_data"][i][2]
+	y3 = result["plotting_data"][i][3]
+	plt.scatter(x, y1)
+	plt.show()
 
 
 print "T: ", temperature
