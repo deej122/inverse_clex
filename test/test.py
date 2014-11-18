d9c3d35117ace0ff1909baaf43bed2f01b811653
kris789ten <kige@umich.edu>
2014-11-18 00:48:42 -0500

    more plotting work

--- test/test.py
+++ test/test.py
@@ -1,5 +1,9 @@
 import json
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plot_ensemble_avg_abs
+import matplotlib.pyplot as plot_ensemble_avg_squared
+import matplotlib.pyplot as plot_ensemble_avg_squares
+
+
 
 json_data = open("plotting_data.json").read()
 
@@ -17,13 +21,32 @@ for i in range(0, length):
 	ensemble_average_absolute.append(result["plotting_data"][i][1])
 	ensemble_average_squared.append(result["plotting_data"][i][2])
 	ensemble_average_squares.append(result["plotting_data"][i][3])
-	x = result["plotting_data"][i][0]
-	y1 = result["plotting_data"][i][1]
-	y2 = result["plotting_data"][i][2]
-	y3 = result["plotting_data"][i][3]
-	plt.scatter(x, y1)
-	plt.show()
 
+fig_ensemble_avg_abs = plot_ensemble_avg_abs.figure()
+fig_ensemble_avg_squared = plot_ensemble_avg_squared.figure()
+fig_ensemble_avg_squares = plot_ensemble_avg_squares.figure()
+
+plot1 = fig_ensemble_avg_abs.add_subplot(111)
+plot1.set_title('Ensemble Average Absolute Value vs Temp')
+plot1.set_xlabel('temperature (K)')
+plot1.set_ylabel('Ensemble Average Absolute Value')
+plot1.plot(temperature, ensemble_average_absolute, 'o')
+
+plot2 = fig_ensemble_avg_squared.add_subplot(111)
+plot2.set_title('Ensemble Average Squared vs Temp')
+plot2.set_xlabel('temperature (K)')
+plot2.set_ylabel('Ensemble Average Squared')
+plot2.plot(temperature, ensemble_average_squared, 'o')
+
+plot3 = fig_ensemble_avg_squares.add_subplot(111)
+plot3.set_title('Ensemble Average Squares vs Temp')
+plot3.set_xlabel('temperature (K)')
+plot3.set_ylabel('Ensemble Average Squares')
+plot3.plot(temperature, ensemble_average_squares, 'o')
+
+plot_ensemble_avg_abs.show()
+plot_ensemble_avg_squared()
+plot_ensemble_avg_squares()
 
 print "T: ", temperature
 print "Absolute Average: ", ensemble_average_absolute
