7e4d5da97c30aaccfbf7d94fd49b8195c552fffc
kris789ten <kige@umich.edu>
2014-11-23 09:53:19 -0500

    created calc_ln_Q function and calculated fixed overflow error

--- test/optimization.py
+++ test/optimization.py
@@ -14,36 +14,43 @@ two_ECI = 1
 three_ECI = 1
 ECI_vec = [one_ECI, two_ECI, three_ECI]
 
-temp = mc_data["Temp"]
-delta_corr_count = 0
-site_count = 0
-sum_delta_corr = 0
-#dot_product will be dotting the delta_corr vec and the ECI_vec
-dot_product = 0
-beta = 1/(temp*8.62*10**-5)
-#summation is the sum of the exp(-beta*dot_product) for add different atoms of one site
-summation = 0
-#P is the probability of the atom ending up as the atom we saw, P=1/summation
-P = 0
-#Q is the probablity we see the matrix we found. To avoid multiplying all the P's together, we can say lnQ = ln(sum of P's)
-ln_Q = 0
-e = 2.71821
+#function to calculate ln_Q
+#i put f in front of the variable names to not confuse it with the variables outside of the funtion
+def calc_ln_Q (f_ECI_vec, f_mc_data):
+	"Calculates ln(Q)"
 
-#this for loop finds the information for the matrix after the last pass
-for site_count in mc_data["Data_by_pass"][len(mc_data["Data_by_pass"])-1]["sites"]:
-	#summation needs to be reset to 0 after each site
-	summation = 0 
-	#this for loop iterates over the delta_corr vecs
-	for delta_corr_count in mc_data["Data_by_pass"][len(mc_data["Data_by_pass"])-1]["sites"][site_count]["delta_corrs"]:
-		delta_corr_vec = mc_data["Data_by_pass"][len(mc_data["Data_by_pass"])-1]["sites"][site_count]["delta_corrs"][delta_corr_count]
-		#dot product of delta_corr and ECI_vec
-		for a in range (0, len(delta_corr_vec)-1):
-			dot_product += delta_corr_vec[a]*ECI_vec[a]
-			#Where do we do this minimization? Because ECI_vec is our variable and has to be one of the inputs.
-			result = minimize(dot_product, ECI_vec, method="nelder-mead")
-		#This causes an overflow error
-		summation += e**(dot_product*beta*-1)
-		P = 1/summation
-	ln_Q += P
+	#set up needed variables
+	delta_corr_count = 0
+	site_count = 0
+	#dot_product will be dotting the delta_corr vec and the ECI_vec
+	dot_product = 0
+	temp = f_mc_data["Temp"]
+	beta = 1/(temp*8.62*10**-5)
+	#summation is the sum of the exp(-beta*dot_product) for add different atoms of one site
+	summation = 0
+	#P is the probability of the atom ending up as the atom we saw, P=1/summation
+	P = 0
+	#Q is the probablity we see the matrix we found. To avoid multiplying all the P's together, we can say lnQ = ln(sum of P's)
+	f_ln_Q = 0
+	e = 2.71821
+	last_pass_info = f_mc_data["Data_by_pass"][len(f_mc_data["Data_by_pass"])-1]
 
-print "P = ", P
+	#this loop finds the information for the matrix after the last pass
+	for site_count in last_pass_info["sites"]:
+		#reset summation to 0 after each site
+		summation = 0
+		#this loop iterates over the delta_corr vecs for a single site
+		#for delta_corr_count in f_mc_data["Data_by_pass"]["Species"]:
+		for delta_corr_count in range (0, 1):
+			#recieving an error that says site_count is a dict :(
+			site_count = int(site_count)
+			delta_corr_count = int(delta_corr_count)
+			delta_corr_vec = last_pass_info["sites"][site_count]["delta_corrs"][delta_corr_count]
+			for a in len(delta_corr_vec)-1:
+				dot_product += delta_corr_vec[a]*f_ECI_vec[a]
+		f_ln_Q += (dot_product*beta)
+
+	return f_ln_Q
+
+ln_Q = calc_ln_Q(ECI_vec, mc_data)
+print "ln(Q) = ", ln_Q
