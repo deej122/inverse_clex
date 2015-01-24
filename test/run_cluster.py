e71b3b6d7bb510b446642d7a4b7e0a4e6408b721
DJ Jagannathan <desjag@gmail.com>
2015-01-24 14:36:05 -0500

    working on flux code

--- test/run_cluster.py
+++ test/run_cluster.py
@@ -1,5 +1,6 @@
 import numpy as np
 import json
+import os
 
 ECI_vec = [[1,0,0], [-1,0,0], [.3,-.7,.05]]
 transition_temp = 30000
@@ -18,17 +19,23 @@ for ECI in ECI_vec:
 				json.dump({'Temperature': temperature, 'Dimensions': dimensions, 'Species': species, 'ECI': ECI, 'Passes': num_passes}, outfile, indent=4)
 			# run in the cluster
 			# copy all files over to flux and compile files (make execute_mc)
-			# import pbs
-			# make sure in monte_carlo.cpp that you create data files with unique names/in new directories
-			# chdir(path) --> create a new folder every time running with different ECI_conditions and THEN submit the job
-			# job = pbs.templates.NonPrismsJob(command='./execute_mc')
+			import pbs
+			eci_index = str(ECI_vec.index(ECI))
+			dim_index = str(dim_small_vec.index(small_dimensions))
+			temp_index = str(temp_vec.index(temperature))
+			directory = "small_" + eci_index + "_" + dim_index + "_" + temp_index
+			print directory
+			if not os.path.exists(directory):
+				os.makedirs(directory)
+			chdir(directory) # --> create a new folder every time running with different ECI_conditions and THEN submit the job
+			job = pbs.templates.NonPrismsJob(command='./execute_mc')
 			# test it first
 			# chmod to edit file/directory permissions
 				# to delete: qdel jobid / pstat --delete/abort
 			    # -a, --all             Select all jobs in database
 			    # --range MINID MAXID   A range of Job IDs (inclusive) to query or operate on
 			    # --recent DD:HH:MM:SS  Select jobs created or modified within given amout of time
-			# job.submit() --> submits job
+			job.submit() # --> submits job
 		print "ECI: ", ECI
 		print "T: ", temperature
 		print "Passes: ", num_passes
@@ -39,6 +46,24 @@ for ECI in ECI_vec:
 			with open("ECI_conditions.json", "w") as outfile:
 				json.dump({'Temperature': temperature, 'Dimensions': dimensions, 'Species': species, 'ECI': ECI, 'Passes': num_passes}, outfile, indent=4)
 			# run in the cluster
+			# copy all files over to flux and compile files (make execute_mc)
+			import pbs
+			eci_index = str(ECI_vec.index(ECI))
+			dim_index = str(dim_small_vec.index(small_dimensions))
+			temp_index = str(temp_vec.index(temperature))
+			directory = "large_" + eci_index + "_" + dim_index + "_" + temp_index
+			print directory
+			if not os.path.exists(directory):
+				os.makedirs(directory)
+			chdir(directory) # --> create a new folder every time running with different ECI_conditions and THEN submit the job
+			job = pbs.templates.NonPrismsJob(command='./execute_mc')
+			# test it first
+			# chmod to edit file/directory permissions
+				# to delete: qdel jobid / pstat --delete/abort
+			    # -a, --all             Select all jobs in database
+			    # --range MINID MAXID   A range of Job IDs (inclusive) to query or operate on
+			    # --recent DD:HH:MM:SS  Select jobs created or modified within given amout of time
+			job.submit() # --> submits job
 		print "ECI: ", ECI
 		print "T: ", temperature
 		print "Passes: ", num_passes
