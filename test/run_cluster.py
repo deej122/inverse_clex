d7cbf098569f2c0ac62e07424fded8645d8b1fcd
Kristen Kimie Ige <kige@flux-login1.engin.umich.edu>
2015-02-05 23:56:39 -0500

    asdf

--- test/run_cluster.py
+++ test/run_cluster.py
@@ -4,9 +4,9 @@ import os
 import pbs
 
 
-ECI_vec = [[1,0,0], [-1,0,0], [.3,-.7,.05]]
+ECI_vec = [[1,0,0]]#, [-1,0,0], [.3,-.7,.05]]
 transition_temp = 30000
-temp_mult = [.001, .01, .1, .2, .5, .9, 1, 1.1, 1.5, 10, 100, 1000]
+temp_mult = [.001, .01]#, .1, .2, .5, .9, 1, 1.1, 1.5, 10, 100, 1000]
 temp_vec = [transition_temp*multiple for multiple in temp_mult]
 num_passes = 100000
 dim_small_vec = [10, 20, 50]
@@ -44,7 +44,7 @@ for ECI in ECI_vec:
 		print "T: ", temperature
 		print "Passes: ", num_passes
 		print "Dimension: ", small_dimensions
-	for large_dimensions in dim_large_vec:
+	if False: #for large_dimensions in dim_large_vec:
 		dimensions = [large_dimensions, large_dimensions]
 		for temperature in temp_vec:
 			# run in the cluster
