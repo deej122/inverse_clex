8455099554d5c02374c0febd539df6039620798f
Kristen Ige <kige@umich.edu>
2015-02-13 13:43:32 -0500

    asdfd

--- test/run_cluster.py
+++ test/run_cluster.py
@@ -4,9 +4,9 @@ import os
 import pbs
 
 
-ECI_vec = [[1,0,0]]#, [-1,0,0], [.3,-.7,.05]]
+ECI_vec = [[1,0,0], [-1,0,0], [.3,-.7,.05]]
 transition_temp = 30000
-temp_mult = [.001, .01]#, .1, .2, .5, .9, 1, 1.1, 1.5, 10, 100, 1000]
+temp_mult = [.001, .01, .1, .2, .5, .9, 1, 1.1, 1.5, 10, 100, 1000]
 temp_vec = [transition_temp*multiple for multiple in temp_mult]
 num_passes = 100000
 dim_small_vec = [10, 20, 50]
