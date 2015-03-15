262d69e79a2c7b6d71e843c69691f45e52ab1968
Kristen Ige <kige@umich.edu>
2015-03-15 10:54:04 -0400

    asdf

--- test/optimization.py
+++ test/optimization.py
@@ -44,14 +44,14 @@ three_ECI = 0
 ECI_vec = [one_ECI, two_ECI, three_ECI]
 
 ln_Q = calc_ln_Q(ECI_vec, mc_data)
-print "ln(Q) = ", ln_Q
+#print "ln(Q) = ", ln_Q
 optimization_NM = minimize(calc_ln_Q, ECI_vec, method='nelder-mead', args=(mc_data_list,), options={'xtol': 1e-8})
 # should calculate derivative and pass in using jas = "derivative" property
 #right now it calculates derivative using first differences approximation
 # optimization_BFGS = minimize(calc_ln_Q, ECI_vec, method='BFGS', args=(mc_data,))
 
 #prints original ECI vector
-print "ECI vector = ", ECI_vec[0], ECI_vec[1], ECI_vec[2]
+#print "ECI vector = ", ECI_vec[0], ECI_vec[1], ECI_vec[2]
 
 #display returned optimization stats
 print "Nelder-Mead Approximation: ", optimization_NM
