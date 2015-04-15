3e39352a42668caf357025bdfdf91e86589756ae
DJ Jagannathan <desjag@gmail.com>
2015-04-14 21:17:39 -0400

    removing some commented out stuff

--- test/unknown_optimization.py
+++ test/unknown_optimization.py
@@ -56,66 +56,6 @@ for filename in str(sys.argv):
 #create full mc_files for passing into 
 mc_full_data_list = create_complete_mc(mc_partial_data_list)
 
-#moved to constructor
-################################################
-# for filename in str(sys.argv):
-# 	file_name = filename.split("_")
-# 	size = file_name[0]
-# 	ECI_index = file_name[1]
-# 	dim_index = file_name[2]
-# 	temp_index = file_name[3]
-
-# 	parent = os.getcwd()
-# 	os.chdir(filename)
-
-# 	#create dict consisting of a list of files
-# 	file_name = filename
-# 	_file = {file_name:[]}
-
-# 	file_info = {}
-# 	if size == "large":
-# 		file_info["Dimension"] = dim_large_vec_key[dim_index]
-# 	else if size == "small":
-# 		file_info["Dimension"] = dim_small_vec_key[dim_index]
-# 	file_info["Temperature"] = temp_vec_key[temp_index]
-# 	file_info["ECI"] = ECI_vec_key[ECI_index]
-
-# 	#create a constructor that, given a set of files, outputs a json object (conditions.json stuff from lnQ) for an [i][j] corresponding to [file][pass]
-# 		#would be a lot easier to use and understand
-# 		#run some tests and make sure this works well first
-
-# 	json_mc_data = open("monte_carlo_calcs.json").read()
-# 	mc_data = json.loads(json_mc_data)
-# 	for _pass in mc_data["Data_by_pass"]:
-# 		#create dict consisting of list of passes
-# 		index = _pass["pass"]
-# 		current_pass = {index:[]}
-# 		#for each site in every object
-# 		for atom in _pass["sites"]
-# 			#generate random number
-# 			random = random.random()
-# 			#add ~known_percent of atoms to known subset for this snapshot
-# 			if random < percent_known:
-# 				#mc_partial_data_list is a list of files > list of passes > list of known atoms (coordinates and current occupant)
-# 				#create object of known_atom and add it to list for this pass
-# 				known_atom = {"coordinate": atom["coordinate"], "current_occupant": atom["current_occupant"]}
-# 				current_pass[index].append(known_atom)
-# 			#otherwise ignore the atom
-# 			else:
-# 				current_pass = current_pass
-# 		#add pass to list in this file
-# 		file_info["Known_Sites"] = []
-# 		file_info["Known_Sites"].append(current_pass)
-
-# 		_file[file_name].append(file_info)
-# 	os.chdir(parent)
-
-# 	#add completed list of known items in each pass within a file, to a list of all
-# 	mc_partial_data_list.append(_file)
-################################################
-
-#   mc_data = [mc_data]
-
 one_ECI = 1
 two_ECI = 0
 three_ECI = 0
